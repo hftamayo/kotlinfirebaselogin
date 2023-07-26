591f416d8c075ed90b992eaf413f546efdea55c9
hftamayo <hftamayo@gmail.com>
2023-07-26 15:59:51 -0600

    validating input part1

--- app/src/main/java/com/hftamayo/kotlinfirebaselogin/SignInActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinfirebaselogin/SignInActivity.kt
@@ -3,14 +3,21 @@ package com.hftamayo.kotlinfirebaselogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hftamayo.kotlinfirebaselogin.databinding.ActivitySignInBinding
 
 class SignInActivity : BaseActivity() {
     private var binding:ActivitySignInBinding? = null
+    private lateinit var auth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+        auth = Firebase.auth
 
         binding?.tvRegister?.setOnClickListener {
             startActivity(Intent(this,SignUpActivity::class.java))
@@ -21,4 +28,31 @@ class SignInActivity : BaseActivity() {
         }
 
     }
+
+    private fun sinInUser(){
+        val email = binding?.etSinInEmail?.text.toString()
+        val password = binding?.etSinInPassword?.text.toString()
+        if(validateForm(email, password)) {
+            auth.signInWithEmailAndPassword(email, password)
+        }
+
+    }
+
+    private fun validateForm(email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(email) && !Patterns.EMAIL_ADDRESS.matcher(email).matches() -> {
+                binding?.tilEmail?.error = "Enter valid email address"
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                binding?.tilPassword?.error = "Enter name"
+                false
+            }
+            else -> {true}
+        }
+
+    }
+
+
+
 }
