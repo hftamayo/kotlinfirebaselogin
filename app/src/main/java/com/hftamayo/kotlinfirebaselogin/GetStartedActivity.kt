0e7f37624d683b988a4814ed2c229ca5e04192ba
hftamayo <hftamayo@gmail.com>
2023-07-27 10:54:18 -0600

    adding auth session evaluate criteria

--- app/src/main/java/com/hftamayo/kotlinfirebaselogin/GetStartedActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinfirebaselogin/GetStartedActivity.kt
@@ -3,6 +3,8 @@ package com.hftamayo.kotlinfirebaselogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.hftamayo.kotlinfirebaselogin.databinding.ActivityGetStartedBinding
 
 class GetStartedActivity : AppCompatActivity() {
@@ -14,6 +16,12 @@ class GetStartedActivity : AppCompatActivity() {
 
         binding?.cvGetStarted?.setOnClickListener{
             startActivity(Intent(this,SignInActivity::class.java))
+            finish()
+        }
+
+        val auth = Firebase.auth
+        if(auth.currentUser != null){
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
     }
