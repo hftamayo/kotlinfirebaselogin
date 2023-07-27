d44d1d9cf83ad05c9cde3aeda744ed0df72d12e5
hftamayo <hftamayo@gmail.com>
2023-07-27 10:54:29 -0600

    method for sign out

--- app/src/main/java/com/hftamayo/kotlinfirebaselogin/MainActivity.kt
+++ app/src/main/java/com/hftamayo/kotlinfirebaselogin/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.hftamayo.kotlinfirebaselogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hftamayo.kotlinfirebaselogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        auth = Firebase.auth
+
+        binding?.btnSignOut?.setOnClickListener{
+            if(auth.currentUser != null){
+                auth.signOut()
+                startActivity(Intent(this, GetStartedActivity::class.java))
+                finish()
+            }
+        }
     }
 }
